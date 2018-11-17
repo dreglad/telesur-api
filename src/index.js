ce62aa8690dd84b94748391d933d17e4dc407462
David Regla <dreglad@gmail.com>
2018-11-16 18:27:49 -0600

    :chart_with_upwards_trend: Add Apollo Engine

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 require('dotenv').config()
 const { GraphQLServer } = require('graphql-yoga')
+const { ApolloEngine } = require('apollo-engine')
 const resolvers = require('./resolvers')
 
-const server = new GraphQLServer({
+const graphQLServer = new GraphQLServer({
   typeDefs: './src/schema.graphql',
   resolvers
 })
 
-server.start({
-  port: process.env.PORT
-}, () => console.log(`Server is running on http://localhost:${process.env.PORT}`))
+if (!process.env.APOLLO_ENGINE_KEY) {
+  // Without Apollo Engine
+  graphQLServer.start({
+    port: process.env.PORT
+  }, () => console.log(`Server is running on http://localhost:${process.env.PORT}`))
+} else {
+  // With Apollo Engine
+  const engine = new ApolloEngine({
+    apiKey: process.env.APOLLO_ENGINE_KEY,
+  })
+
+  const httpServer = graphQLServer.createHttpServer({
+    tracing: true,
+    cacheControl: {
+      defaultMaxAge: 60
+    }
+  })
+
+  engine.listen({
+    httpServer,
+    port: process.env.PORT,
+    graphqlPaths: ['/'],
+  }, () => console.log(`Server with Apollo Engine is running on http://localhost:${process.env.PORT}`))
+}
