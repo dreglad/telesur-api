59f60d5636cb9cd79e89b93f657f304d285211eb
David Regla <dreglad@gmail.com>
2018-11-21 23:04:01 -0600

    :wrench: Change articleSection and articleSections schema and resolvers to forward query received from client to the server

--- src/news/resolvers.js
+++ src/news/resolvers.js
@@ -2,8 +2,17 @@ const Query = {
   articles (_, args, { db }, info) {
     return db.query.articles(args, info)
   },
+
   articleSections (_, args, { db }, info) {
     return db.query.articleSections(args, info)
+  },
+
+  articleSection (_, args, { db }, info) {
+    return db.query.articleSection(args, info)
+  },
+
+  articlesCount(_, args, { prisma }) {
+    return prisma.articlesConnection().aggregate().count()
   }
 }
 
