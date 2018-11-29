3822c80128e70a5b0f596f1aa7805f065230b6da
David Regla <dreglad@gmail.com>
2018-11-29 02:47:08 -0600

    :sparkles: Add relay connectors to schema and resolvers

--- src/videos/resolvers.js
+++ src/videos/resolvers.js
@@ -1,4 +1,7 @@
 const fetch = require('node-fetch')
+const { UserInputError } = require('apollo-server-core');
+const { GraphQLError } = require('graphql');
+const { isEmpty } = require('lodash');
 const { toQueryString, setCacheHintFromRes } = require('../utils')
 const differenceInDays = require('date-fns/difference_in_days')
 const parse = require('date-fns/parse')
@@ -13,103 +16,34 @@ const {
 
 const Query = {
   clips: async (_, args, { service }) => {
-    const { results } = await clipsFetch(service, args.where, args.first, args.skip);
+    const results = await clipsFetch(service, args);
     return results.map(mapClip);
   },
   clipsConnection: async (_, args, { service }) => {
-    const { totalCount } = await clipsFetch(service, args.where, args.first, args.skip);
-    return { aggregate: { count: totalCount } };
+    const count = await clipsFetch(service, { ...args, return: 'count' });
+    return { aggregate: { count } };
   },
-  clip: async (_, { id }, { service }, { cacheControl }) => {
-    const clip = mapClip(await restFetch(service, `/clip/${id}/`, { detalle: 'completo' }));
-    if (cacheControl) {
-      const maxAge = differenceInDays(new Date(), parse(clip.date)) ? 86400 : 60
-      cacheControl.setCacheHint({ maxAge })
-    }
-    return clip;
-  },
+  clip: buildRestObjectResolver('clip', mapClip),
 
-  series: (_, args, { service }, { cacheControl }) => {
-    return new Promise((resolve, reject) => {
-      const params = { limit: args.first, offset: args.skip || 0 }
-      restFetch(service, `/programa/`, params).catch(reject).then(res => {
-        setCacheHintFromRes(res, cacheControl)
-        res.json().then(programas => { resolve(programas.map(mapSerie)) }).catch(reject)
-      })
-    })
-  },
-  serie: (_, { id }, { service }, { cacheControl }) => {
-    return new Promise((resolve, reject) => {
-      restFetch(service, `/programa/${id}/`).catch(reject).then(res => {
-        setCacheHintFromRes(res, cacheControl)
-        res.json().then(programa => { resolve(mapSerie(programa)) }).catch(() => { resolve(null) })
-      })
-    })
-  },
+  series: buildRestListResolver('programa', mapSerie),
+  seriesConnection: buildRestConnectionResolver('programa', mapSerie),
+  serie: buildRestObjectResolver('programa', mapSerie),
 
-  genres: (_, args, { service }, { cacheControl }) => {
-    return new Promise((resolve, reject) => {
-      const params = { limit: args.first, offset: args.skip || 0 }
-      restFetch(service, `/tipo_clip/`, params).catch(reject).then(res => {
-        res.json().then(tipos => { resolve(tipos.map(mapGenre)) }).catch(reject)
-      })
-    })
-  },
-  genre: (_, { id }, { service }) => {
-    return new Promise((resolve, reject) => {
-      restFetch(service, `/tipo_clip/${id}/`).catch(reject).then(res => {
-        res.json().then(tipo => { resolve(mapGenre(tipo)) }).catch(() => { resolve(null) })
-      })
-    })
-  },
+  genres: buildRestListResolver('tipo_clip', mapGenre),
+  genresConnection: buildRestConnectionResolver('tipo_clip', mapGenre),
+  genre: buildRestObjectResolver('tipo_clip', mapGenre),
 
-  categories: (_, args, { service }) => {
-    return new Promise((resolve, reject) => {
-      const params = { limit: args.first, offset: args.skip || 0 }
-      restFetch(service, `/categoria/`, params).catch(reject).then(res => {
-        res.json().then(categorias => { resolve(categorias.map(mapCategory)) }).catch(reject)
-      })
-    })
-  },
-  category: (_, { id }, { service }) => {
-    return new Promise((resolve, reject) => {
-      restFetch(service, `/categoria/${id}/`).catch(reject).then(res => {
-        res.json().then(categoria => { resolve(mapCategory(categoria)) }).catch(() => { resolve(null) })
-      })
-    })
-  },
+  categories: buildRestListResolver('categoria', mapCategory),
+  categoriesConnection: buildRestConnectionResolver('categoria', mapCategory),
+  category: buildRestObjectResolver('categoria', mapCategory),
 
-  correspondents: (_, args, { service }) => {
-    return new Promise((resolve, reject) => {
-      const params = { pais: args.country, limit: args.first, offset: args.skip || 0 }
-      restFetch(service, `/corresponsal/`, params).catch(reject).then(res => {
-        res.json().then(corresponsales => { resolve(corresponsales.map(mapCorrespondent)) }).catch(reject)
-      })
-    })
-  },
-  correspondent: (_, { id }, { service }) => {
-    return new Promise((resolve, reject) => {
-      restFetch(service, `/corresponsal/${id}/`).catch(reject).then(res => {
-        res.json().then(categoria => { resolve(mapCorrespondent(categoria)) }).catch(() => { resolve(null) })
-      })
-    })
-  },
+  correspondents: buildRestListResolver('corresponsal', mapCorrespondent),
+  correspondentsConnection: buildRestConnectionResolver('corresponsal', mapCorrespondent),
+  correspondent: buildRestObjectResolver('corresponsal', mapCorrespondent),
 
-  topics: (_, args, { service }) => {
-    return new Promise((resolve, reject) => {
-      const params = { limit: args.first, offset: args.skip || 0 }
-      restFetch(service, `/tema/`, params).catch(reject).then(res => {
-        res.json().then(temas => { resolve(temas.map(mapTopic)) }).catch(reject)
-      })
-    })
-  },
-  topic: (_, { id }, { service }) => {
-    return new Promise((resolve, reject) => {
-      restFetch(service, `/tema/${id}/`).catch(reject).then(res => {
-        res.json().then(tema => { resolve(mapTopic(tema)) }).catch(() => { resolve(null) })
-      })
-    })
-  }
+  topics: buildRestListResolver('tema', mapTopic),
+  topicsConnection: buildRestConnectionResolver('tema', mapTopic),
+  topics: buildRestObjectResolver('tema', mapTopic)
 }
 
 const typeResolvers = {
@@ -142,24 +76,44 @@ const typeResolvers = {
   },
 };
 
-const restFetch = ({ videoRestUrl }, path, params) => {
-  return fetch(`${videoRestUrl}${path}/?${toQueryString(params)}`)
-    .then(res => res.json())
-    .then(data => {
-      if (!Array.isArray(data) && Object.keys(data).includes('totalCount')) {
-        return data
-      } else {
-        return data
-      }
-    });
+function buildRestConnectionResolver(resource, mapper) {
+  return async (_, { first, skip }, { service }) => {
+    const params = { limit: first || 1000, offset: skip || 0, return: 'count' };
+    const count = await restFetch(service, `/${resource}/`, params);
+    return { aggregate: { count } };
+  };
 }
 
-const clipsFetch = (service, where = {}, first, skip) => {
+function buildRestListResolver(resource, mapper) {
+  return async (_, { first, skip }, { service }) => {
+    const params = { limit: first, offset: skip || 0 };
+    const results = await restFetch(service, `/${resource}/`, params);
+    return results.map(mapper);
+  };
+}
+
+function buildRestObjectResolver(resource, mapper) {
+  return async (_, { id }, { service }) => {
+    // Validate input
+    if (id.trim().length < 2) {
+      throw new GraphQLError('Invlaid or empty id provided');
+    }
+    // load data
+    const result = await restFetch(service, `/${resource}/${id}/`);
+    return (result && !isEmpty(result))
+      ? mapper(result)
+      : null
+  };
+};
+
+const clipsFetch = (service, args) => {
   const relation = (rel, isNull) => typeof isNull !== 'undefined' ? isNull && 'es_nulo' || 'no_es_nulo' : rel
+  const { where = {} } = args;
   return restFetch(service, '/clip/', {
-    counts: true,
-    limit: first,
-    offset: skip,
+    limit: args.first,
+    offset: args.skip,
+    return: args.return,
+    counts: args.counts,
     tipo: where.episodesOfSerie ? 'programa' : where.genre,
     programa: where.episodesOfSerie || where.serie,
     country_code: where.country,
@@ -169,6 +123,10 @@ const clipsFetch = (service, where = {}, first, skip) => {
   })
 };
 
+const restFetch = ({ videoRestUrl }, path, params) => {
+  return fetch(`${videoRestUrl}${path}/?${toQueryString(params)}`)
+    .then(res => res.json().catch(() => null));
+};
 
 module.exports = {
   Query,
