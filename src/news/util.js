0fbaf89757f366f5a68eca0cdfdff0cc889060f1
David Regla <dreglad@gmail.com>
2018-12-05 02:14:50 -0600

    :sparkles: Add ability to scrape RSS as well as HTML for article links

--- src/news/util.js
+++ src/news/util.js
@@ -7,18 +7,21 @@ async function crawlLinks(url) {
   const origin = new URL(url).origin;
   let urls = await fetch(url)
     .then(res => res.text())
-    .then(text => cheerio.load(text))
-    .then(
-      $ => $('a')
-        .map((i, el) => $(el).prop('href'))
-        .get()
-    )
+    .then(text => cheerio.load(text, {xmlMode: true }))
+    .then($ => {
+      const rssItems = $('channel item link');
+      if (rssItems.length) {
+        return rssItems.map((i, el) => $(el).text()).get();
+      } else {
+        return $('a').map((i, el) => $(el).prop('href')).get();
+      }
+    });
 
   urls = [ ...new Set(urls) ]
-    .filter(url => !url.startsWith('#') && !url.startsWith('http:'))
+    .filter(url => !url.startsWith('#'))
     .map(url => new URL(url, origin).href);
 
-  return urls
+  return urls.map(url => url.replace('http://', 'https://'))
 }
 
 module.exports = {
