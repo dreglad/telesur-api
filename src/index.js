df96a5fa3cfbb38957b4ff07e8391afa4f5275ad
David Regla <dreglad@gmail.com>
2019-01-03 14:47:25 -0600

    :arrow_up: Replace graphql-yoga with apollo-server@2

--- src/index.js
+++ src/index.js
@@ -1,107 +1,55 @@
-const { ApolloEngine } = require('apollo-engine');
-const { GraphQLServer, PubSub } = require('graphql-yoga');
-const binding = require('prisma-binding');
-const { Prisma } = require('./generated/prisma-client');
-const resolvers = require('./resolvers');
+const { importSchema } = require('graphql-import')
+const binding = require('prisma-binding')
+const { ApolloEngine } = require('apollo-engine')
+const { ApolloServer } = require('apollo-server')
+const { Prisma } = require('./generated/prisma-client')
+const resolvers = require('./resolvers')
 
 const port = parseInt(process.env.PORT, 10) || 4000;
 
-const endpoint = process.env.PRISMA_ENDPOINT || 'http://127.0.0.1:4466';
+const prismaEndpoint = process.env.PRISMA_ENDPOINT || 'http://127.0.0.1:4466';
 
 const db = new binding.Prisma({
   typeDefs: 'src/generated/graphql-schema/prisma.graphql',
-  endpoint
+  endpoint: prismaEndpoint
 });
 
-const prisma = new Prisma({ endpoint });
+const prisma = new Prisma({ endpoint: prismaEndpoint });
 
-const pubsub = new PubSub();
-
-const graphQLServer = new GraphQLServer({
-  typeDefs: ['src/schema/schema.graphql'],
+const server = new ApolloServer({
+  typeDefs: importSchema('src/schema/schema.graphql'),
   resolvers,
-  context: async req => ({
-    ...req,
+  context: async ({req, res}) => ({
     db,
     prisma,
-    pubsub,
-    cache: {}, // TODO: Use Facebook's Dataloader
+    token: req.headers['auth-token'],
     service: await prisma.service({
-      name: req.request.headers['x-service-name'] || process.env.DEFAULT_SERVICE_NAME
+      name: req.headers['x-service-name'] || process.env.DEFAULT_SERVICE_NAME
     })
-  })
+  }),
+  tracing: true,
+  cacheControl: {
+    defaultMaxAge: 60
+  },
+  engine: {
+    apiKey: process.env.ENGINE_API_KEY,
+    generateClientInfo: ({ request }) => {
+      const headers = request.http & request.http.headers;
+      if (headers) {
+        return {
+          clientName: headers['apollo-client-name'],
+          clientVersion: headers['apollo-client-version'],
+        };
+      } else {
+        return {
+          clientName: "Unknown Client",
+          clientVersion: "Unversioned",
+        };
+      }
+    },
+  }
 });
 
-if (process.env.ENGINE_API_KEY) {
-  // With Apollo Engine
-  const httpServer = graphQLServer.createHttpServer({
-    tracing: true,
-    cacheControl: {
-      defaultMaxAge: 60
-    }
-  });
-
-  const engine = new ApolloEngine({
-    apiKey: process.env.ENGINE_API_KEY,
-    // logging: {
-    //   level: "DEBUG"
-    // },
-    generateClientInfo: ({ request }) => {
-      const headers = request.headers;
-      return {
-        clientName: headers && headers['client-name'] || 'Unknown Client',
-        clientVersion: headers && headers['client-version'] || 'Unversioned',
-      };
-    },
-    // Specify behavior for how the Engine Proxy should connect to the
-    // GraphQL origin (your Node GraphQL server). While the Proxy does
-    // support multiple origins, most users will only put one origin
-    // in this array.
-    frontends: [{
-      overrideGraphqlResponseHeaders: {
-        'Access-Control-Allow-Origin': '*'
-      }
-    }],
-    origins: [
-      {
-        // If you are using the apollo-link-batch-http package to combine
-        // multiple GraphQL requests into a single HTTP request, set this
-        // to ensure that the Engine Proxy sends them to your Node web
-        // server as a single request as well. (If you don't set this,
-        // the Proxy will split it apart into individual HTTP requests.)
-        supportsBatch: true,
-        // Amount of time to wait for your Node GraphQL server to return
-        // a response before timing out. Defaults to "30s". Specified as
-        // a string containing a number and a unit such as "ms" or "s".
-        requestTimeout: "120s",
-        // HTTP-specific options. (The options above also apply to other
-        // origin types such as Lambda.)
-      }
-    ]
-  })
-
-  engine.on("error", err => {
-    console.log("There was an error starting the server or Engine.");
-    console.error(err);
-    process.exit(1);
-  })
-
-  engine.listen({
-    port,
-    httpServer,
-    graphqlPaths: ['/']
-  },
-    () => {
-      console.log(`Server with Apollo Engine is running on http://localhost:${process.env.PORT}`)
-    }
-  )
-} else {
-  // Without Apollo Engine
-  graphQLServer.start({
-    port
-  },
-    () => {
-      console.log(`Server is running on http://localhost:${process.env.PORT}`)
-    }
-  )
-}
+server.listen().then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+});
