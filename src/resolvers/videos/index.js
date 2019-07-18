2235717e900e30eb208896ddf764386da6dd7d38
David Regla <dreglad@gmail.com>
2019-07-18 16:29:23 -0500

    :sparkes: Add mocked me query

--- src/resolvers/videos/index.js
+++ src/resolvers/videos/index.js
@@ -2,9 +2,18 @@ const { merge } = require('lodash')
 const Playlist = require('./Playlist')
 const Video = require('./Video')
 
+const User = {
+  Query: {
+    me (_, args, { db, service }, info) {
+      return null;
+    }
+  }
+}
+
 const resolvers = merge(
   Playlist,
-  Video
+  Video,
+  User
 )
 
 module.exports = resolvers;
